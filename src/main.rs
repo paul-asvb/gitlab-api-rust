1829148abe4213f2fc57b53d78e12e2919f9c850
paul work <paulwork@Pauls-MBP.fritz.box>
2020-12-15 23:17:33 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+extern crate gitlab_api as gitlab;
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+
+fn main() {
+    dotenv().ok();
+
+    let gl = gitlab::GitLab::new(&"gitlab.com", &"GITLAB_TOKEN_XXXXXXX").unwrap();
+
+    // Get GitLab's version.
+    let gitlab_version = gl.version().unwrap();
+    println!("gitlab_version: {:?}", gitlab_version);
+
+
+    // Low level methods
+
+    // Get projects, owned by authenticated user and which are archived.
+    let projects = gl.projects().owned().archived(true).list().unwrap();
+    println!("projects: {:?}", projects);
+
+    // Get groups owned by authenticated user.
+    let owned_groups = gl.groups().owned().list().unwrap();
+    println!("owned_groups: {:?}", owned_groups);
+
+    // Get closed issues.
+    let closed_issues = gl.issues().state(gitlab::issues::State::Closed).list().unwrap();
+    println!("closed_issues: {:?}", closed_issues);
+
+
+    // Higher level methods
+
+    // Get a specific project
+    let project = gl.get_project("nbigaouette1", "gitlab-api-rs").chain_err(|| "cannot get project")?;
+
+    // Get a specific issue
+    let issue = gl.get_issue("nbigaouette1", "gitlab-api-rs", 1).chain_err(|| "cannot get issue")?;
+
+    // Get a specific merge request
+    let merge_request = gl.get_merge_request("nbigaouette1", "gitlab-api-rs", 1).chain_err(|| "cannot get merge_request")?;
+}
